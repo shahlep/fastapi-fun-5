bcd811eee1b77bdf7c3c5b55c6d0ce6f1f234de0
Shah rahman <toshah.rahman@gmail.com>
2022-05-26 10:09:02 +0300

    fastapi fun 7

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ def admin():
     return f'welcome to admin page'
 
 
+# route with dynamic path params with type
 @app.get('/user/{username}')
 def user(username: str):
     return f'this page is for user {username}'
 
 
+# route with query params with deafult value and type
 @app.get('/product')
 def product(id: int = 1, price: float = 10.0):
     return f'product with an id: {id} and price: {price}'
+
+
+# route with dynamic path params and query
+@app.get('/profile/{userid}/comments')
+def profile(userid: int, commentid: int):
+    return f'profile page with userid {userid} and corresponding comment {commentid}'
