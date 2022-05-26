6940e68500b4c86635625ea797e7b025e32258da
Shah rahman <toshah.rahman@gmail.com>
2022-05-26 10:52:32 +0300

    fastapi fun 9

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Profile(BaseModel):
+    name: str
+    email: str
+
 
 app = FastAPI()
 
@@ -13,19 +20,6 @@ def admin():
     return f"welcome to admin page"
 
 
-# route with dynamic path params with type
-@app.get("/user/{username}")
-def user(username: str):
-    return f"this page is for user {username}"
-
-
-# route with query params with deafult value and type
-@app.get("/product")
-def product(id: int = 1, price: float = 10.0):
-    return f"product with an id: {id} and price: {price}"
-
-
-# route with dynamic path params and query
-@app.get("/profile/{userid}/comments")
-def profile(userid: int, commentid: int):
-    return f"profile page with userid {userid} and corresponding comment {commentid}"
+@app.post('/adduser')
+def add_user(profile: Profile):
+    return {"data":profile}
